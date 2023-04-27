f3038ce2ca3523a599025baeb36aade8a9279291
dsbahia <129111782+dsbahia@users.noreply.github.com>
2023-04-27 21:12:24 +0100

    Completed sumNumbers in arrays.js, stuck on final sortByLastLetter test

--- src/arrays.js
+++ src/arrays.js
@@ -19,49 +19,83 @@ const addToArray = (element, array) => {
 };
 
 const addToArray2 = (element, array) => {
-  let newArray = [...array, element]; // ... = spread operator
+  // test states we need to return new array
+  let newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    newArray.push(array[i]);
+  }
+  newArray.push(element);
   return newArray;
 };
 
 const removeNthElement = (index, array) => {
-  array.splice(index, 1);
+  array.splice(index, 1); // removes 1 element from array, splice is modifying the original array so no need to use return. The "1" is the number of elements to remove
 };
 
 const numbersToStrings = numbers => {
-  // your code here
+  // this converts every number in an array to a string using arrow function (=>)
+  // .map creates a new array
+  // toString() method returns a string representing the object. 
+  return numbers.map(number => number.toString());
 };
 
 const uppercaseWordsInArray = strings => {
-  // your code here
+  // does the same as the above, only this time adding uppercase. The toUpperCase() method returns the value of the string converted to uppercase
+  return strings.map(letter => letter.toString().toUpperCase());
 };
 
 const reverseWordsInArray = strings => {
-  // your code here
+  // using a for loop instead of .map - i = iterator 
+  const reversedArray = [];
+  for (let i = 0; i < strings.length; i++) {
+    const reversedString = strings[i].split('').reverse().join('');
+    reversedArray.push(reversedString);
+  }
+  return reversedArray;
 };
 
 const onlyEven = numbers => {
-  // your code here
+  const evenNumbers = numbers.filter(num => num % 2 === 0);
+  return evenNumbers;
 };
 
 const removeNthElement2 = (index, array) => {
-  // your code here
+  if (index < 0 || index >= array.length) {
+    return array;
+  }
+  const newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (i !== index) {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray;
 };
 
 const elementsStartingWithAVowel = strings => {
-  // your code here
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+  const vowelStrings = strings.filter(string => {
+    const firstChar = string[0]?.toLowerCase();
+    return vowels.includes(firstChar);
+  });
+  return vowelStrings;
 };
 
 const removeSpaces = string => {
-  // your code here
+  return string.split(' ').join('');
 };
 
 const sumNumbers = numbers => {
-  // your code here
+  let s = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    s += numbers[i]
+  }
+  return s;
 };
 
 const sortByLastLetter = strings => {
   // your code here
-};
+}
 
 module.exports = {
   getNthElement,
