bd5a6912d9ef72c10068e4eb93dea5799369ddd9
dsbahia <129111782+dsbahia@users.noreply.github.com>
2023-04-29 20:59:55 +0100

    Completed findHondas test in objects.js

--- src/objects.js
+++ src/objects.js
@@ -4,31 +4,49 @@ const createPerson = (name, age) => {
 };
 
 const getName = object => {
-  // your code here
+  return object.name;
 };
 
 const getProperty = (property, object) => {
-  // your code here
+  return object[property];
 };
 
 const hasProperty = (property, object) => {
-  // your code here
+  const keys = Object.keys(object);
+  for (let i = 0; i < keys.length; i++) {
+    if (keys[i] === property.toString()) {
+      return true;
+    }
+  }
+  return false;
 };
 
 const isOver65 = person => {
-  // your code here
+  return person.age > 65;
 };
 
 const getAges = people => {
-  // your code here
+  const ages = [];
+  for (let i = 0; i < people.length; i++) {
+    ages.push(people[i].age);
+  }
+  return ages;
 };
 
 const findByName = (name, people) => {
-  // your code here
+  let match;
+  for (let i = 0; i < people.length; i++) {
+    if (people[i].name === name) {
+      match = people[i];
+      break;
+    }
+  }
+  return match;
 };
 
 const findHondas = cars => {
-  // your code here
+  const listOfHondas = cars.filter(car => car.manufacturer === 'Honda');
+  return listOfHondas;
 };
 
 const averageAge = people => {
