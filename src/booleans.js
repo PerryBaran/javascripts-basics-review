3eb50b649fe013b30732f9439d2b6a0990fc3f27
Holly Mathieson <hjmathieson@gmail.com>
2023-04-30 00:30:06 -0300

    first boolean test done

--- src/booleans.js
+++ src/booleans.js
@@ -1,62 +1,66 @@
 function negate(a) {
-  // your code here
-};
+  if (a) {
+    return false;
+  } else {
+    return true;
+  }
+}
 
 function both(a, b) {
   // your code here
-};
+}
 
 function either(a, b) {
   // your code here
-};
+}
 
 function none(a, b) {
   // your code here
-};
+}
 
 function one(a, b) {
   // your code here
-};
+}
 
 function truthiness(a) {
   // your code here
-};
+}
 
 function isEqual(a, b) {
   // your code here
-};
+}
 
 function isGreaterThan(a, b) {
   // your code here
-};
+}
 
 function isLessThanOrEqualTo(a, b) {
   // your code here
-};
+}
 
 function isOdd(a) {
   // your code here
-};
+}
 
 function isEven(a) {
   // your code here
-};
+}
 
 function isSquare(a) {
   // your code here
-};
+}
 
 function startsWith(char, string) {
   // your code here
-};
+}
 
 function containsVowels(string) {
   // your code here
-};
+}
 
 function isLowerCase(string) {
   // your code here
-};
+}
 
 module.exports = {
   negate,
