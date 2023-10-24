fc13ed00a53e71e6f9fb2ecf275ae43fb2cea4f8
Ashley Daniels <ashdaniels25gmail.com>
2023-10-24 16:54:35 +0100

    numbers completed

--- src/numbers.js
+++ src/numbers.js
@@ -1,45 +1,45 @@
-const add = (a, b) => {
+function add (a, b) {
   return a + b;
 }
 
-const subtract = (a, b) => {
+function subtract (a, b) {
  return a - b;
 }
 
-const multiply = (a, b) => {
+function multiply (a, b)  {
 return a * b; 
 }
 
-const divide = (a, b) => {
+function divide (a, b) {
   return a / b;
 }
 
-const power = (a, b) => {
+function power  (a, b) {
   return a ** b;
 }
 
-const round = (a) => {
+function round  (a) {
 return Math.round (a); 
 }
 
-function roundUp (a) {
-  // your code here
+function roundUp  (a)  {
+  return Math.ceil (a);
 }
 
-function roundDown (a) {
-  // your code here
+function roundDown  (a)  {
+  return Math.floor (a);
 }
 
-function absolute (a) {
-  // your code here
+function absolute (a)  {
+  return Math.abs (a);
 }
 
-function quotient (a, b) {
-  // your code here
+function quotient (a, b)  {
+return Math.trunc(a / b); 
 }
 
 function remainder (a, b) {
-  // your code here
+  return a % b;
 }
 
 module.exports = {
