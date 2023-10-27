157ab1dac474766b970bb061c5a7a93b81b564d2
Ben-H123 <ben_h321@protonmail.com>
2023-10-27 21:04:37 +0100

    finished booleans with errors

--- src/booleans.js
+++ src/booleans.js
@@ -1,61 +1,111 @@
-function negate(a) {
-  // your code here
+const negate =(a) => {
+return !a
 };
 
-function both(a, b) {
-  // your code here
+const both = (a, b) => {
+  if (a && b) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
-function either(a, b) {
-  // your code here
+const either = (a, b) => {
+  if (a || b) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
-function none(a, b) {
-  // your code here
+const none = (a, b) => {
+  if (a || b) {
+    return false;
+  } else {
+    return true;
+  }
 };
 
-function one(a, b) {
-  // your code here
+function one (a, b) {
+  if ((a && b) && (!a && !b)) {
+    return false;
+  } else {
+    return true;
+  }
 };
 
-function truthiness(a) {
-  // your code here
+function truthiness (a) {
+const falsy = [false, undefined, 0, null, undefined, NaN];
+  if (a = falsy) {
+    return false; 
+  } else {
+    return true;
+  }
 };
 
-function isEqual(a, b) {
-  // your code here
+const isEqual = (a, b) => {
+  if (a === b) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 function isGreaterThan(a, b) {
-  // your code here
+  if (a > b) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 function isLessThanOrEqualTo(a, b) {
-  // your code here
+  if (a <= b) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 function isOdd(a) {
-  // your code here
+if (a%2 == 1) {
+  return true;
+} else {
+  return false;
+}
+
 };
 
 function isEven(a) {
-  // your code here
+  if (a%2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
-function isSquare(a) {
-  // your code here
+const isSquare = (a) => {
+const num = Math.sqrt(a);
+return a.isInteger(a);
 };
 
 function startsWith(char, string) {
-  // your code here
+ if (char === string[0]){
+  return true;
+ } else {
+  return false;
+ }
 };
 
-function containsVowels(string) {
-  // your code here
+const containsVowels = (string) => {
+  let lowerCase = string.toLowerCase();
+  const vowelRegex = /[aeiou]/i;
+  return vowelRegex.test(string);
+
 };
 
 function isLowerCase(string) {
-  // your code here
+  return /^[a-z0-9]+$/.test(string);
 };
 
 module.exports = {
