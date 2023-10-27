8a22355494704cdcd2cfe749e29e6d2436da2b20
Ben-H123 <ben_h321@protonmail.com>
2023-10-27 14:12:08 +0100

    all strings done

--- src/strings.js
+++ src/strings.js
@@ -1,26 +1,31 @@
-function sayHello (string) {
-  // your code here
+const sayHello = (string) => {
+let greeting = 'Hello, ';
+  return (greeting + string + "!")
 };
 
-function uppercase (string) {
-  // your code here
+const uppercase = (string) => {
+  return string.toUpperCase();
 };
 
-function lowercase (string) {
-  // your code here
+const lowercase = (string) => {
+  return string.toLowerCase();
 };
 
-function countCharacters (string) {
-  // your code here
+const countCharacters = (string) => {
+  return string.length;
 };
 
-function firstCharacter (string) {
-  // your code here
+const firstCharacter = (string) => {
+  return string[0];
 };
 
-function firstCharacters (string, n) {
-  // your code here
-};
+const firstCharacters = (string, n) => {
+  if (string.length >4) {
+  return string.slice(0,4);
+} else {
+  return string.slice(0,2);
+}
+}
 
 module.exports = {
   sayHello,
